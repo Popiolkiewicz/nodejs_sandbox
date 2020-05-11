29de5ced1804c464072b42c40a8ccc18858234f4
Hubert Popiołkiewicz <popiolkiewicz.it@gmail.com>
2020-05-11 18:12:57 +0200

    MVC intro

--- express_server/app.js
+++ express_server/app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
@@ -18,20 +18,16 @@ app.set('views', 'views')
 // app.set('view engine', 'pug');
 // app.set('views', 'views')
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error')
 
 // request parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
-app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
-  // With pug:
-  res.status(404).render('page-not-found', { pageTitle: 'Page not found' });
-});
+app.use(errorController.getPageNotFound);
 
 app.listen(3000); 
